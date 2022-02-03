3b578f3980eaca47a2e8f40b4b2410d4ff476eb0
Hajime Suzuki <suzuki.hajime.s@gmail.com>
2022-02-03 22:16:01 +0900

    add rustfmt::skip around clap args

--- src/main.rs
+++ src/main.rs
@@ -81,6 +81,7 @@ OPTIONS:
 ";
 
 fn main() {
+    #[rustfmt::skip]
     let m = App::new("zd")
         .version("0.0.1")
         .about("streamed binary processor")
@@ -91,173 +92,39 @@ fn main() {
         .setting(AppSettings::DontDelimitTrailingValues)
         .setting(AppSettings::InferLongArgs)
         .args([
-            Arg::new("inputs")
-                .help("input files")
-                .value_name("input.bin")
-                .multiple_occurrences(true)
-                .default_value("-"),
-            Arg::new("in-format")
-                .short('i')
-                .long("in-format")
-                .help("input format signatures [b]")
-                .takes_value(true)
-                .number_of_values(1),
-            Arg::new("out-format")
-                .short('o')
-                .long("out-format")
-                .help("output format signatures [xxx]")
-                .takes_value(true)
-                .number_of_values(1),
-            Arg::new("patch-format")
-                .short('j')
-                .long("patch-format")
-                .help("patchfile format signatures [xxx]")
-                .takes_value(true)
-                .number_of_values(1),
-            Arg::new("cat")
-                .short('c')
-                .long("cat")
-                .help("concat all input streams into one with W-byte words (default) [1]")
-                .takes_value(true)
-                .min_values(0)
-                .possible_values(["1", "2", "4", "8", "16"])
-                .default_missing_value("1"),
-            Arg::new("zip")
-                .short('z')
-                .long("zip")
-                .help("zip all input streams into one with W-byte words")
-                .takes_value(true)
-                .min_values(0)
-                .possible_values(["1", "2", "4", "8", "16"])
-                .default_missing_value("1")
-                .conflicts_with("cat"),
-            Arg::new("seek")
-                .short('s')
-                .long("seek")
-                .help("skip first N bytes and clear stream offset [0]")
-                .takes_value(true)
-                .number_of_values(1),
-            Arg::new("range")
-                .short('r')
-                .long("range")
-                .help("drop bytes out of the range [0:inf]")
-                .takes_value(true),
-            Arg::new("patch")
-                .short('p')
-                .long("patch")
-                .help("patch the input stream with the patchfile (after all of the above)")
-                .takes_value(true),
-            Arg::new("patch-op")
-                .short('b')
-                .long("patch-op")
-                .help("do binary operation on patching")
-                .takes_value(true),
-            Arg::new("op")
-                .short('B')
-                .long("op")
-                .help("apply unary bit operation to every W-byte word")
-                .takes_value(true),
-            Arg::new("splice")
-                .short('L')
-                .long("splice")
-                .help("output only bytes at the indices in every W-byte word")
-                .takes_value(true),
-            Arg::new("shuffle")
-                .short('Y')
-                .long("shuffle")
-                .help("shuffle bytes with the indices in W-byte words")
-                .takes_value(true),
-            Arg::new("multiply")
-                .short('X')
-                .long("multiply")
-                .help("multiply bitmatrix to every W-byte word")
-                .takes_value(true),
-            Arg::new("substitute")
-                .short('S')
-                .long("substitute")
-                .help("substitute word with the map")
-                .takes_value(true),
-            Arg::new("histogram")
-                .short('H')
-                .long("histogram")
-                .help("compute a histogram of bits B:E in W-byte words on finite input stream")
-                .takes_value(true),
-            Arg::new("sort")
-                .short('O')
-                .long("sort")
-                .help("sort finite input stream of W-byte words by bits from N to M")
-                .takes_value(true),
-            Arg::new("cluster")
-                .short('D')
-                .long("cluster")
-                .help("cluster binary pattern by bits B:E in W-byte words")
-                .takes_value(true),
-            Arg::new("reverse")
-                .short('R')
-                .long("reverse")
-                .help("reverse finite input stream of W-byte words")
-                .takes_value(true),
-            Arg::new("transpose")
-                .short('T')
-                .long("transpose")
-                .help("transpose finite input stream with the shape and its indices")
-                .takes_value(true),
-            Arg::new("width")
-                .short('w')
-                .long("width")
-                .help("slice into N bytes (default) [16]")
-                .takes_value(true),
-            Arg::new("match")
-                .short('m')
-                .long("match")
-                .help("slice out every matches whose Hamming distance is less than K from the pattern")
-                .takes_value(true),
-            Arg::new("regex")
-                .short('e')
-                .long("regex")
-                .help("slice out every matches with regular expression (in PCRE)")
-                .takes_value(true),
-            Arg::new("margin")
-                .short('G')
-                .long("margin")
-                .help("extend every slice to left and right by N and M bytes [0:0]")
-                .takes_value(true),
-            Arg::new("merge")
-                .short('M')
-                .long("merge")
-                .help("merge two slices whose overlap is longer than N bytes (after extension) [0]")
-                .takes_value(true),
-            Arg::new("pad")
-                .short('P')
-                .long("pad")
-                .help("pad slices left and right by N and M bytes (after extension and merging) [0:0]")
-                .takes_value(true),
-            Arg::new("group")
-                .short('l')
-                .long("group")
-                .help("group N slices (default) [1]")
-                .takes_value(true),
-            Arg::new("uniq")
-                .short('u')
-                .long("uniq")
-                .help("group slices whose bits from B to E are the same [0:inf]")
-                .takes_value(true),
-            Arg::new("map")
-                .short('f')
-                .long("map")
-                .help("apply shell command to every grouped slices []")
-                .takes_value(true),
-            Arg::new("reduce")
-                .short('d')
-                .long("reduce")
-                .help("pass outputs of --map command to another as input files []")
-                .takes_value(true),
+            Arg::new("inputs").help("input files").value_name("input.bin").multiple_occurrences(true).default_value("-"),
+            Arg::new("in-format").short('i').long("in-format").help("input format signatures [b]").takes_value(true).number_of_values(1),
+            Arg::new("out-format").short('o').long("out-format").help("output format signatures [xxx]").takes_value(true).number_of_values(1),
+            Arg::new("patch-format").short('j').long("patch-format").help("patchfile format signatures [xxx]").takes_value(true).number_of_values(1),
+            Arg::new("cat").short('c').long("cat").help("concat all input streams into one with W-byte words (default) [1]").takes_value(true).min_values(0).possible_values(["1", "2", "4", "8", "16"]).default_missing_value("1"),
+            Arg::new("zip").short('z').long("zip").help("zip all input streams into one with W-byte words").takes_value(true).min_values(0).possible_values(["1", "2", "4", "8", "16"]).default_missing_value("1").conflicts_with("cat"),
+            Arg::new("seek").short('s').long("seek").help("skip first N bytes and clear stream offset [0]").takes_value(true).number_of_values(1),
+            Arg::new("range").short('r').long("range").help("drop bytes out of the range [0:inf]").takes_value(true),
+            Arg::new("patch").short('p').long("patch").help("patch the input stream with the patchfile (after all of the above)").takes_value(true),
+            Arg::new("patch-op").short('b').long("patch-op").help("do binary operation on patching").takes_value(true),
+            Arg::new("op").short('B').long("op").help("apply unary bit operation to every W-byte word").takes_value(true),
+            Arg::new("splice").short('L').long("splice").help("output only bytes at the indices in every W-byte word").takes_value(true),
+            Arg::new("shuffle").short('Y').long("shuffle").help("shuffle bytes with the indices in W-byte words").takes_value(true),
+            Arg::new("multiply").short('X').long("multiply").help("multiply bitmatrix to every W-byte word").takes_value(true),
+            Arg::new("substitute").short('S').long("substitute").help("substitute word with the map").takes_value(true),
+            Arg::new("histogram").short('H').long("histogram").help("compute a histogram of bits B:E in W-byte words on finite input stream").takes_value(true),
+            Arg::new("sort").short('O').long("sort").help("sort finite input stream of W-byte words by bits from N to M").takes_value(true),
+            Arg::new("cluster").short('D').long("cluster").help("cluster binary pattern by bits B:E in W-byte words").takes_value(true),
+            Arg::new("reverse").short('R').long("reverse").help("reverse finite input stream of W-byte words").takes_value(true),
+            Arg::new("transpose").short('T').long("transpose").help("transpose finite input stream with the shape and its indices").takes_value(true),
+            Arg::new("width").short('w').long("width").help("slice into N bytes (default) [16]").takes_value(true),
+            Arg::new("match").short('m').long("match").help("slice out every matches whose Hamming distance is less than K from the pattern").takes_value(true),
+            Arg::new("regex").short('e').long("regex").help("slice out every matches with regular expression (in PCRE)").takes_value(true),
+            Arg::new("margin").short('G').long("margin").help("extend every slice to left and right by N and M bytes [0:0]").takes_value(true),
+            Arg::new("merge").short('M').long("merge").help("merge two slices whose overlap is longer than N bytes (after extension) [0]").takes_value(true),
+            Arg::new("pad").short('P').long("pad").help("pad slices left and right by N and M bytes (after extension and merging) [0:0]").takes_value(true),
+            Arg::new("group").short('l').long("group").help("group N slices (default) [1]").takes_value(true),
+            Arg::new("uniq").short('u').long("uniq").help("group slices whose bits from B to E are the same [0:inf]").takes_value(true),
+            Arg::new("map").short('f').long("map").help("apply shell command to every grouped slices []").takes_value(true),
+            Arg::new("reduce").short('d').long("reduce").help("pass outputs of --map command to another as input files []").takes_value(true),
             Arg::new("help").short('h').long("help").help("print help (this) message"),
             Arg::new("version").short('v').long("version").help("print version information"),
-            Arg::new("verbose")
-                .short('V')
-                .long("verbose")
-                .help("print stream diagram and verbose logs"),
+            Arg::new("verbose").short('V').long("verbose").help("print stream diagram and verbose logs"),
         ])
         .get_matches();
 
