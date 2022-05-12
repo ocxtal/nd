6d8fc244360f883984d89598c1ad60e1ff39f39a
Hajime Suzuki <suzuki.hajime.s@gmail.com>
2022-05-12 23:10:38 +0900

    dispatch hex / binary (raw) formatters

--- src/text/formatter/mod.rs
+++ src/text/formatter/mod.rs
@@ -9,8 +9,37 @@ use super::InoutFormat;
 use crate::filluninit::FillUninit;
 use crate::segment::Segment;
 
+fn format_binary(_offset: usize, _min_width: usize, stream: &[u8], segments: &[Segment], buf: &mut Vec<u8>) {
+    for s in segments {
+        buf.fill_uninit(s.len, |dst: &mut [u8]| {
+            let src = &stream[s.as_range()];
+            unsafe { std::ptr::copy_nonoverlapping(src.as_ptr(), dst.as_mut_ptr(), s.len) };
+            Ok(s.len)
+        })
+        .unwrap();
+    }
+}
+
+fn format_hex(offset: usize, min_width: usize, stream: &[u8], segments: &[Segment], buf: &mut Vec<u8>) {
+    // TODO: unroll the loop
+    for s in segments {
+        let src = &stream[s.as_range()];
+        let reserve = 8 * ((s.len + 15) & !15) + 8 * 32;
+
+        buf.fill_uninit(reserve, |dst: &mut [u8]| {
+            let offset = offset + s.pos;
+            let len = unsafe { format_line(dst, src, offset, s.len.max(min_width)) };
+            Ok(len)
+        })
+        .unwrap();
+    }
+}
+
+type FormatLines = fn(usize, usize, &[u8], &[Segment], &mut Vec<u8>);
+
 pub struct TextFormatter {
     format: InoutFormat,
+    format_lines: FormatLines,
     offset: (usize, usize),
     min_width: usize,
 }
@@ -19,6 +48,7 @@ impl TextFormatter {
     pub fn new(format: &InoutFormat, offset: (usize, usize), min_width: usize) -> Self {
         TextFormatter {
             format: *format,
+            format_lines: if format.is_binary() { format_binary } else { format_hex },
             offset,
             min_width,
         }
@@ -29,18 +59,7 @@ impl TextFormatter {
     }
 
     pub fn format_segments(&self, offset: usize, stream: &[u8], segments: &[Segment], buf: &mut Vec<u8>) {
-        // TODO: unroll the loop
-        for s in segments {
-            let src = &stream[s.as_range()];
-            let reserve = 8 * ((s.len + 15) & !15) + 8 * 32;
-
-            buf.fill_uninit(reserve, |dst: &mut [u8]| {
-                let offset = self.offset.0 + offset + s.pos;
-                let len = unsafe { format_line(dst, src, offset, s.len.max(self.min_width)) };
-                Ok(len)
-            })
-            .unwrap();
-        }
+        (self.format_lines)(self.offset.0 + offset, self.min_width, stream, segments, buf);
     }
 }
 
