a3c8df6d6409671ce785092c8a13c8aff739c642
Hajime Suzuki <suzuki.hajime.s@gmail.com>
2022-08-12 20:11:04 +0900

    cleanup unused files (#8)
    done

--- src/drain/mod.rs
+++ src/drain/mod.rs
@@ -3,14 +3,8 @@
 
 mod patch;
 mod scatter;
-// mod trans;
 
 pub use self::patch::PatchDrain;
 pub use self::scatter::ScatterDrain;
-// pub use self::trans::TransparentDrain;
-
-// pub trait StreamDrain {
-//     fn consume_segments(&mut self) -> std::io::Result<usize>;
-// }
 
 // end of mod.rs
