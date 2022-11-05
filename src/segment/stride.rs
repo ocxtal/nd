b7f6692525e3ceb3aefabe5af13afb1263254b72
Hajime Suzuki <suzuki.hajime.s@gmail.com>
2022-11-05 19:10:14 +0900

    done

--- src/segment/stride.rs
+++ src/segment/stride.rs
@@ -111,8 +111,9 @@ struct Phase {
 }
 
 struct ConstSegments {
-    init_state: InitState, // (phase_offset, min_bytes_to_escape)
-    phase: Phase,          // (curr_phase, prev_phase)
+    segments: Vec<Segment>, // precalculated segment array
+    init_state: InitState,  // (phase_offset, min_bytes_to_escape)
+    phase: Phase,           // (curr_phase, prev_phase)
     is_eof: bool,
     in_lend: (usize, usize), // (len, count)
     tail_offset_margin: usize,
@@ -120,8 +121,6 @@ struct ConstSegments {
     open_ended: (bool, bool),
     pitch: usize,
     span: usize,
-
-    segments: Vec<Segment>, // precalculated segment array
 }
 
 impl ConstSegments {
@@ -217,7 +216,7 @@ impl ConstSegments {
             is_eof: false,
             in_lend: (0, 0),
             tail_offset_margin: offset_margin.1,
-            tail_reserved_bytes: offset_margin.1.saturating_sub(span),
+            tail_reserved_bytes: offset_margin.1.saturating_sub(span) + pitch,
             open_ended,
             pitch,
             span,
@@ -330,6 +329,8 @@ impl ConstSegments {
         }
 
         let n_extra = self.count_segments(next_tail.saturating_sub(len + self.pitch));
+        // let n_extra = std::cmp::min(self.segments.len(), n_extra);
+
         let next_tail = next_tail - n_extra * self.pitch;
         let count = self.segments.len() - n_extra;
 
